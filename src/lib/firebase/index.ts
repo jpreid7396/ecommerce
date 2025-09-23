739ba0041814fea146819180d2ae7c717fcc0924
Julia Reid <julia.reid22@gmail.com>
2025-09-23 18:17:03 -0400

    fixing compile error for rc

--- src/lib/firebase/index.ts
+++ src/lib/firebase/index.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { getRemoteConfig, getValue, fetchAndActivate } from 'firebase/remote-config';
+import { getRemoteConfig, getValue, fetchAndActivate, type RemoteConfig } from 'firebase/remote-config';
+import { getAnalytics } from 'firebase/analytics'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -9,27 +10,35 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 
 // Initialize Firebase Auth and export it
 export const auth = getAuth(app)
 export { app as firebaseApp }
 
-export const remoteConfig = getRemoteConfig(app);
-remoteConfig.settings = {
-  minimumFetchIntervalMillis: 3600000,
-  fetchTimeoutMillis: 60000,
-};
-remoteConfig.defaultConfig = {
-  darkmode: 'false',
-};
+let remoteConfig: RemoteConfig | undefined;
+if (typeof window !== 'undefined') {
+  remoteConfig = getRemoteConfig(app);
+  remoteConfig.settings = {
+    minimumFetchIntervalMillis: 3600000,
+    fetchTimeoutMillis: 60000,
+  };
+  remoteConfig.defaultConfig = {
+    darkmode: 'false',
+  };
+}
 
 export async function getDarkModeRemoteConfig() {
+  if (typeof window === 'undefined' || !remoteConfig) return false;
   await fetchAndActivate(remoteConfig);
   const darkModeValue = getValue(remoteConfig, 'darkmode').asString();
   return darkModeValue === 'true';
 }
+export { remoteConfig };
